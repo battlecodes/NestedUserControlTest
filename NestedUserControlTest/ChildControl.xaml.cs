61e24786f43b728be947f4f392bf5878fb6ecaf9
battlecodes <calvin.algos@gmail.com>
2022-07-25 16:49:28 -0400

    Add project files.

---
+++ NestedUserControlTest/ChildControl.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NestedUserControlTest
+{
+    /// <summary>
+    /// Interaction logic for ChildControl.xaml
+    /// </summary>
+    public partial class ChildControl : UserControl
+    {
+
+        public static readonly DependencyProperty TitleProperty =
+            DependencyProperty.Register("Title", typeof(string),
+                typeof(ChildControl), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty FieldTextProperty =
+            DependencyProperty.Register("FieldText", typeof(string),
+                typeof(ChildControl), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty ClearCommandProperty =
+            DependencyProperty.Register("ClearCommand", typeof(ICommand),
+                typeof(ChildControl));
+
+
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+
+        public string FieldText
+        {
+            get { return (string)GetValue(FieldTextProperty); }
+            set { SetValue(FieldTextProperty, value); }
+        }
+
+
+        public ICommand ClearCommand
+        {
+            get { return (ICommand)GetValue(ClearCommandProperty); }
+            set { SetValue(ClearCommandProperty, value); }
+        }
+
+
+        public ChildControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ NestedUserControlTest/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NestedUserControlTest
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+
+        private string title = "Field text: ";
+        private string fieldText = string.Empty;
+
+
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                OnPropertyChanged(nameof(Title));
+            }
+        }
+
+
+        public string FieldText
+        {
+            get => fieldText;
+            set
+            {
+                fieldText = value;
+                OnPropertyChanged(nameof(FieldText));
+            }
+        }
+
+
+        public ICommand ClearCommand { get; }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+        public MainViewModel()
+        {
+            ClearCommand = new RelayCommand(new Action<object>(ClearFields));
+        }
+
+
+
+        private void ClearFields(object obj)
+        {
+            //FieldText = string.Empty;
+            FieldText = "test";
+        }
+
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
